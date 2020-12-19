3a6e6e50c568755084c3c8aeb414393b216696b9
vedshiva <74661276+vedshiva@users.noreply.github.com>
2020-12-19 15:16:46 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -58,13 +58,10 @@ function draw() {
 
 }
 
-function keyPressed() { 
-
-	if (keyCode === up_arrow){
-
-		Matter.Body.applyForce (ball.body,ball.position.{x:85,y:-85})
-
-	}
-
-}
-
+function keyPressed() {
+		if (keyCode === UP_ARROW) {
+  
+		  Matter.Body.applyForce(ball.body,ball.body.position,{x:130,y:-50});
+	  
+		}
+  }
